81d7bd7132d5c2626f17d5183834665596e49eaf
adriel23 <adrielcolombo23@gmail.com>
2024-08-06 10:53:55 -0300

    avances gh-pages-2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://candeyadri.github.io/candeyadri.git/'
+  base:'https://adriel2323.github.io/cande-adri.git/'
 })
