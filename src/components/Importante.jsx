6796cb37fe26610717886d5531f781aa8545487c
adriel23 <adrielcolombo23@gmail.com>
2024-08-08 09:41:48 -0300

    correcciones 8-8_77

--- src/components/Importante.jsx
+++ src/components/Importante.jsx
@@ -16,7 +16,7 @@ export default function Importante() {
                                 }
                                 className=" w-[75vw] sm:w-[40vw] mx-auto -mt-5" src="./images/carteles/importante.svg" alt="" />
                             <section >
-                                <ul className=" space-y-3 mt-10 sm:w-[50vw] mx-auto">
+                                <ul className=" space-y-6 mt-10 sm:w-[50vw] mx-auto bg-verde">
                                     <motion.li
                                         initial={{ opacity: 0 }}
                                         whileInView={{ opacity: 1 }}
@@ -30,8 +30,11 @@ export default function Importante() {
                                         className="conteiner_info">
                                         <img className="icono_info " src="./images/acta.svg" alt="" />
                                         <div className="font-serif text-right">
-                                            <img className="h-[50%]"  src="./images/frases/civil.svg" alt="" />
-                                            <p className="-mt-5">Posible fecha:<br />
+                                            <div className="div_img_infoImportante">
+
+                                                <img   src="./images/frases/civil.svg" alt="" />
+                                            </div>
+                                            <p className="-mt-2">Posible fecha:<br />
                                             del 12 al 14 <br />
                                             de febrero del 2025</p>
                                         </div>
@@ -49,8 +52,11 @@ export default function Importante() {
                                         className="conteiner_info">
                                         <img  className="icono_info h-[80%] sm:h-[20vh]" src="./images/iglesia.svg" alt="" />
                                         <div className="font-serif text-right">
-                                            <img className="h-[50%]" src="./images/frases/ceremonia.svg" alt="" />
-                                            <p>15 de febrero del 2025</p>
+                                            <div className="div_img_infoImportante">
+
+                                                <img className="" src="./images/frases/ceremonia.svg" alt="" />
+                                            </div>
+                                            <p className="-mt-2">15 de febrero del 2025</p>
                                             <p>20:00hs - Lavalle 930</p>
                                         </div>
                                     </motion.li>
@@ -67,9 +73,11 @@ export default function Importante() {
                                         }
                                         className="conteiner_info">
                                         <img className="h-[80%] icono_info" src="./images/iconos/fiesta_icono.svg" alt="" />
-                                        <div className="font-serif text-right">
-                                            <img className="h-[50%]" src="./images/frases/fiesta.svg" alt="" />
-                                            <p>Al finalizar la<br />
+                                        <div className="font-serif text-right ">
+                                            <div className="div_img_infoImportante">
+                                                <img className="" src="./images/frases/fiesta.svg" alt="" />
+                                            </div>
+                                            <p className="-mt-2">Al finalizar la<br />
                                             ceremonia-Club Social<br />
                                             Guardias Nacionales 50</p>
                                         </div>
