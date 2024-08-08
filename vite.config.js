77f3449f2b58034b8c07e6435eeade5c7aec3961
adriel23 <adrielcolombo23@gmail.com>
2024-08-08 07:47:39 -0300

    correcciones 8-8_2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://adriel2323.github.io/cande-adri/'
+  base:'https://candeyadri.github.io/invitacion/'
 })
